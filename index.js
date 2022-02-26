af9b58ed2b33deb57a30fdc4b36b0b40af40ebd8
Tyler Monaghan <33797221+tymonaghan@users.noreply.github.com>
2022-02-26 15:05:01 -0600

    add links - no styling

--- index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { render } from "react-dom";
-
-render(
-  <div>Hello, this is React. Five tacos, please.</div>,
-  document.getElementById("app")
-);
---
+++ src/Home.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Tyler's Homepage</h1>
+      <ul>
+        <li>
+          <a href="https://www.github.com/tymonaghan" target="_blank">
+            GitHub
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.linkedin.com/in/tyler-monaghan-dev"
+            target="_blank"
+          >
+            LinkedIn
+          </a>
+        </li>
+        <li>
+          <a href="https://mail.google.com/mail/" target="_blank">
+            Gmail
+          </a>
+        </li>
+        <li>
+          <a href="https://calendar.google.com/calendar/" target="_blank">
+            Calendar
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Home;
