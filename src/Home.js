c45ecf884bd20f1abe2820b9bc6b537ee30e11dc
Tyler Monaghan <33797221+tymonaghan@users.noreply.github.com>
2022-03-08 16:50:47 -0600

    implement react-bootstrap, change links to buttons

--- src/Home.js
+++ src/Home.js
@@ -1,35 +1,25 @@
 import React from "react";
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
 
 const Home = () => {
+  const links = [
+    { name: "github", url: "https://www.github.com/tymonaghan" },
+    { name: "LinkedIn", url: "https://www.linkedin.com/in/tyler-monaghan-dev" },
+    { name: "gmail", url: "https://mail.google.com/mail/" },
+    { name: "google calendar", url: "https://calendar.google.com/calendar/" },
+  ];
   return (
-    <div>
+    <Container>
       <h1>Tyler's Homepage</h1>
-      <ul>
-        <li>
-          <a href="https://www.github.com/tymonaghan" target="_blank">
-            GitHub
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://www.linkedin.com/in/tyler-monaghan-dev"
-            target="_blank"
-          >
-            LinkedIn
-          </a>
-        </li>
-        <li>
-          <a href="https://mail.google.com/mail/" target="_blank">
-            Gmail
-          </a>
-        </li>
-        <li>
-          <a href="https://calendar.google.com/calendar/" target="_blank">
-            Calendar
-          </a>
-        </li>
-      </ul>
-    </div>
+      {links.map((link, key) => {
+        return (
+          <Button className="m-2" key={key} href={link.url} target="_blank">
+            {link.name}
+          </Button>
+        );
+      })}
+    </Container>
   );
 };
 
